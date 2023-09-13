b5aac0aafc0958ef127cc97eb55f1572a1fd11e4
Louis <lst9829@yahoo.com>
2023-09-13 13:05:59 -0600

    added strikethrough and deletion actions for list items

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-let item = "";
 let taskinput = document.getElementById("addText");
-document.querySelector("button").addEventListener('click', (taskinput) => {
-    item = taskinput.value
-    document.querySelector('#to_do_list').innerHTML += `<li> + item + </li>`;
+document.querySelector("button").addEventListener('click', () => {
+    const theList = document.querySelector('ul');
+    const tasking = document.createElement('li');
+    tasking.innerHTML = taskinput.value;
+    const completed = document.createElement("input");
+    taskinput.value = '';
+    theList.appendChild(tasking);
 });
+document.addEventListener('click', (event) => {
+    event.target.classList.add("completed");
+});
+document.addEventListener('dblclick', (event) => {
+    event.target.remove();
+})
+//document.getElementByID('').style = "text-decoration: line-through;"
